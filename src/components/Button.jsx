9fd1eaa8a65fed9deb188527c0b81273e8867bfd
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-10-12 00:08:18 +0200

    Add workflow

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+
+
+import PropTypes from 'prop-types';
+
+const Button = ({ styles }) => (
+  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    Get Started
+  </button>
+);
+
+Button.propTypes = {
+  styles: PropTypes.string.isRequired,
+};
+
+export default Button;
